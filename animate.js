4ca66318946791850aa802523ea865ee8b70e13f
juanalbertomontoya <67620654+juanalbertomontoya@users.noreply.github.com>
2020-11-28 21:08:48 -0500

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,34 @@
+function animate1() {
+  $("#logo-container").removeClass();
+  setTimeout(() => {
+    $("#logo-container").addClass("animated-1");
+  }, 100);
+};
+
+function animate2() {
+  $("#logo-container").removeClass();
+  setTimeout(() => {
+    $("#logo-container").addClass("animated-2");
+  }, 100);
+};
+
+function animate3() {
+  $("#logo-container").removeClass();
+  setTimeout(() => {
+    $("#logo-container").addClass("animated-3");
+  }, 100);
+};
+
+function animate4() {
+  $("#logo-container").removeClass();
+  setTimeout(() => {
+    $("#logo-container").addClass("animated-4");
+  }, 100);
+};
+
+function animate5() {
+  $("#logo-container").removeClass();
+  setTimeout(() => {
+    $("#logo-container").addClass("animated-5");
+  }, 100);
+};
